212a309381c18ce5d831cca246c7bc26d07a3aed
Chris Luna <christian.luna23budgvirtual.udg.mx>
2026-05-25 17:24:07 -0600

    agregue conexion dinamica

--- conexion.php
+++ conexion.php
@@ -1,11 +1,12 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "moonflow_db";
+$host = getenv('MYSQLHOST') ?: "localhost";
+$user = getenv('MYSQLUSER') ?: "root";
+$password = getenv('MYSQLPASSWORD') ?: "";
+$db = getenv('MYSQLDATABASE') ?: "moonflow_db";
+$port = getenv('MYSQLPORT') ?: "3306";
 
-$conn = new mysqli($host, $user, $password, $db);
+$conn = new mysqli($host, $user, $password, $db, $port);
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
@@ -13,4 +14,4 @@ if ($conn->connect_error) {
 
 $conn->set_charset("utf8");
 
-?> 
+?>

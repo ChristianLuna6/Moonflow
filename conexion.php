3dd55b7e89c5e544690ce80f32b865b072c76e6b
Chris Luna <christian.luna23budgvirtual.udg.mx>
2026-05-25 19:36:24 -0600

    aa

--- conexion.php
+++ conexion.php
@@ -1,17 +1,18 @@
 <?php
+mysqli_report(MYSQLI_REPORT_OFF);
 
 $host = getenv('MYSQLHOST') ?: "localhost";
 $user = getenv('MYSQLUSER') ?: "root";
 $password = getenv('MYSQLPASSWORD') ?: "";
-$db = getenv('MYSQLDATABASE') ?: "moonflow_db";
+
+$db = getenv('MYSQLDATABASE') ?: "moonflow_db"; 
 $port = getenv('MYSQLPORT') ?: "3306";
 
 $conn = new mysqli($host, $user, $password, $db, $port);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8");
-
 ?>
